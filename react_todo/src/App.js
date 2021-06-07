e2af10732948a7da02ea3598ada76774fe62c0c2
Onur <onur.ereren998@gmail.com>
2021-06-07 22:44:12 +0200

    Changed App.js and added some styling

--- react_todo/src/App.js
+++ react_todo/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { ToDoProvider } from './contexts/ToDoContext'
+import ToDoList from './components/ToDoList'
+import Form from './components/Form'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    overflow: 'hidden',
+    padding: theme.spacing(0, 3),
+  },
+  paper: {
+    maxWidth: 400,
+    margin: `${theme.spacing(1)}px auto`,
+    padding: theme.spacing(2),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ToDoProvider>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item xs>
+            <Typography component={'div'}>
+              <div className="App" >
+                <h1 className="Title">To Do List</h1>
+                <Form />
+                <ToDoList />
+              </div></Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+    </ToDoProvider>
   );
 }
 
